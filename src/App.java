dc2072293622987450fff0d941709c17494525b4
syumai6842 <syuumai6842@gmail.com>
2024-05-21 15:16:47 +0900

    deleted app.java

--- src/App.java
+++
@@ -1,7 +0,0 @@
-import lib.ExchangeRate;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println(ExchangeRate.ExchangeJPYtoKRW(100));
-    }
-}
